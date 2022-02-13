f095c473df1c2fb48f5072dda03c34c2ecf669ad
shivam_trivedi <shivamtrivedi18@gmail.com>
2022-02-13 18:00:25 +0530

    learning Instance Attribures

--- oops.py
+++ oops.py
@@ -1,13 +1,15 @@
 # oops learning
 class teddy_bear:   #class is defined
-    quantity=200    # properties are defined
-    color=["red","blue"]
+    def __init__(self,name,color):
+        self.name=name
+        self.color=color
 
 
-teddy1 = teddy_bear()       # defining object 1
-teddy2 = teddy_bear()       # defining object 2
-
-print(teddy1.quantity)
-print(teddy2.color)
+teddy1=teddy_bear("ted","red")
+teddy2=teddy_bear("rob","yellow")
+print(teddy1.name)
 print(teddy1.color)
+print(teddy2.name)
+print(teddy2.color)
+
 
