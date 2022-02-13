60845087527fd65e96d29bd4a97457f8087b44ce
shivam_trivedi <shivamtrivedi18@gmail.com>
2022-02-13 13:21:42 +0530

    defining class and object

---
+++ oops.py
@@ -0,0 +1,13 @@
+# oops learning
+class teddy_bear:   #class is defined
+    quantity=200    # properties are defined
+    color=["red","blue"]
+
+
+teddy1 = teddy_bear()       # defining object 1
+teddy2 = teddy_bear()       # defining object 2
+
+print(teddy1.quantity)
+print(teddy2.color)
+print(teddy1.color)
+
