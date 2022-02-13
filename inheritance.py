8166f7fb12aa57451c6dcfd8ad60bb5662b7681a
shivam_trivedi <shivamtrivedi18@gmail.com>
2022-02-13 22:24:23 +0530

    Inheritance example

---
+++ inheritance.py
@@ -0,0 +1,27 @@
+class Student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def get_data(self):
+        self.name=input("Enter the name")
+        self.age=input("Enter the age")
+
+    def print_data(self):
+        print(self.name)
+        print(self.age)
+
+student1=Student("","")
+student1.get_data()
+student1.print_data()
+
+class Science_student(Student):
+    def science(self):
+        print("This is inheritance")
+
+a=Science_student("","")
+a.get_data()
+a.print_data()
+
+
+
