3c21644fb754a7f22ce98459c373a017c86e6015
shivam_trivedi <shivamtrivedi18@gmail.com>
2022-02-15 07:06:29 +0530

    intro to seried

--- data_framce.py
+++ data_framce.py
@@ -1,4 +1,9 @@
+from ast import Index
 from pandas import DataFrame
 data_frame={'Name':["Shivam","Mrity","Suvidha"],'Age':[30,26,39],'Salary': [9,6,2]}  # inthe IDE it will throw an error but works well with in the shell
 frame=DataFrame(data_frame)
-print(frame)
+print(frame)    
+
+from pandas import Series
+s1 = Series(['Apple','Banana','Manago'], index=['a','b','c'])
+print(s1)
