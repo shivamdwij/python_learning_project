d27600368130901ecb55bb4139df0da75ced3e65
shivam_trivedi <shivamtrivedi18@gmail.com>
2022-02-15 06:38:38 +0530

    creating data frame

---
+++ data_framce.py
@@ -0,0 +1,5 @@
+
+from pandas import DataFrame
+data_frame={'Name':["Shivam","Mrity","Suvidha"],'Age':[30,26,39],'Salary': [9,6,2]}  # inthe IDE it will throw an error but works well with in the shell
+frame=DataFrame(data_frame)
+print(frame)
