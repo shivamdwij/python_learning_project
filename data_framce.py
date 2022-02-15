2beb68160ad7302948376ee58866b1545ae1a0fe
shivam_trivedi <shivamtrivedi18@gmail.com>
2022-02-15 06:56:44 +0530

    comments added

--- data_framce.py
+++ data_framce.py
@@ -1,4 +1,3 @@
-
 from pandas import DataFrame
 data_frame={'Name':["Shivam","Mrity","Suvidha"],'Age':[30,26,39],'Salary': [9,6,2]}  # inthe IDE it will throw an error but works well with in the shell
 frame=DataFrame(data_frame)
--- recursion.py
+++ recursion.py
@@ -5,7 +5,7 @@ def factorial(x):
     if (x==1):
         return 1
     else:
-        return x*(factorial(x-1))
+        return x*(factorial(x-1))  # using the function itself in the function
 
 result=factorial(6)
 print(result)   # print factorial of 6, few more............//
