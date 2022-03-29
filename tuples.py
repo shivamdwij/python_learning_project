71bb591bd437fa74e74adcb26ad19321e295fcf1
shivam_trivedi <shivamtrivedi18@gmail.com>
2022-03-30 01:47:38 +0530

    this is set and tuples

---
+++ tuples.py
@@ -0,0 +1,17 @@
+# tuples
+
+t=(1,2,3)
+print(t)
+
+# sets -> Sets are unorderred collections of unique items
+myset= set()
+print(myset)
+
+myset.add(1)
+print(myset)
+
+myset.add(2)
+print(myset)
+
+myset.add(2)  # if we add 2 again, the set will only show 2 once since it only possess unique items
+print(myset)
